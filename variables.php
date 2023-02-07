66a2b09c0c2986b96659bb0cd24815d546381ca6
dadaJunglist <kavalliero@gmail.com>
2023-02-07 21:43:20 +0200

    task 6 ready

--- variables.php
+++ variables.php
@@ -6,9 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task 3</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="website/style.css">
 </head>
 <body>
-  
+
+<h3>tasks 1-2</h3>
+<br>  
+   
    <form action="" method="post">
     <div class="form-group">
             <label for="name">Name:   </label>
@@ -23,14 +27,13 @@
 
 <br>
 <br>
-<?php
+<br>
 
-?>
+<br>
+<h3>task 3</h3>
 <br>
 
 <?php
-    
-    
     $g1 = 5; $g2 = 4; $g3 = 5;
 ?>
 
@@ -56,6 +59,39 @@
   </tr>
 </table>
 
+<br>
+<h3>task 4</h3>
+<br>
+
+<?php
+  
+    $var1="abc";
+    $var2="dfeg";
+    $var3= $var1 . $var2;
+    echo "Strings length sum is: " . strlen($var3);
+    
+?>
+
+<br><br>
+<h3>task 5</h3>
+<br>
+
+<?php
+  $num1 = 298; $num2= 234; $num3 = 46;
+    echo $num1+$num2+$num3;
+?>
+    
+<br><br>
+<h3>task 6</h3>
+<br>
+
+<?php
+echo $_SERVER['HTTP_USER_AGENT'];
+?>
+<br><br>    
+<h3>task 7</h3>
+
+    <?php include "website/footer.php"; ?>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
