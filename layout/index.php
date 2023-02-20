3fa5824a509e30925389bdc7f63554145a203190
dadaJunglist <kavalliero@gmail.com>
2023-02-20 11:22:39 +0200

    class done

--- layout/index.php
+++ layout/index.php
@@ -9,8 +9,27 @@
 <body>
     
 <?php include "header.php"; ?>
+
 <script src="js/site.js"></script>
-<input type="button" onclick="hello()" value="Click Me">
+<input type="button" onclick="hello()" value="Click Me"><br>
+<input type="button" onclick="hello1()" value="Click Me"><br>
+<input type="button" onclick="hello2()" value="Click Me"><br><br>
+
+<input type="button" onclick="add()" value="5 + 6">
+
+<hr>
+<p id="place1" style="color:red; background-color:black;"></p>
+<span id="place2" style="color:black; background-color:red;"></span>
+
+<script>
+document.getElementById("place1").innerHTML = "This will go to place1";
+document.getElementById("place2").innerHTML = "This will go span"
+</script>
+
+<h2> Changing background color</h2>
+<form action="">
+    <input type="color" name="background" onchange="colorChange('background',this.value)">
+</form>
 
 <h1> This is main page</h1>
  
--- layout/js/site.js
+++ layout/js/site.js
@@ -1,4 +1,37 @@
+//function to trigger alert note
+
+//display or like echo
+// document.write
+// windows.alert
+// document.getElementById
+
+// let / Var / name = "" , Const
+
 function hello()
 {
     alert("Welcome to my website");
+}
+
+function hello1()
+{
+    document.write("HEEEELLLOOOOO!!!");
+}
+
+function hello2()
+{
+    window.alert("1");
+    window.alert("2");
+}
+
+//simple function to add two numbers
+
+function add(){
+    document.write(5+6);
+}
+
+function colorChange(where,newColor)
+{
+    if (where == "background")
+        {
+            document.body.style.backgroundColor = newColor;        }
 }
